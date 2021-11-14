c1738660ae1e6d3b70d0023d4f4224dedb3aa0ba
Patrick Bucher <patrick.bucher@mailbox.org>
2021-11-14 10:43:49 +0100

    compute grades (instead of parsing Excel formulae)

--- gradedocs/parser.py
+++ gradedocs/parser.py
@@ -2,20 +2,32 @@ def parse(ws, first_criteria_col):
     ws_rows = iter(ws)
     title_row = next(ws_rows)
     criteria = extract_criteria(title_row, first_criteria_col)
-    print(criteria)
+
     reference_row = next(ws_rows)
     reference_result = extract_result(reference_row, criteria)
+    max_points = sum(reference_result['scores'].values())
+    max_grade = compute_grade(reference_result, max_points)
+    reference_result['max_points'] = max_points
+    reference_result['grade'] = max_grade
+
     student_results = []
     for row in ws_rows:
-        student_results.append(extract_result(row, criteria))
+        student_result = extract_result(row, criteria)
+        grade = compute_grade(student_result, max_points)
+        student_result['max_points'] = max_points
+        student_result['grade'] = grade
+        student_results.append(student_result)
+
     return (reference_result, student_results)
 
 
-def extract_criteria(title_row, from_col):
-    criteria = {}
-    for i, criterium in enumerate(title_row[from_col:]):
-        criteria[i + from_col] = criterium.value
-    return criteria
+def round_to(x, granularity=0.01):
+    return round(x * 1/granularity) * granularity
+
+
+def compute_grade(result, max_points):
+    got_points = sum(result['scores'].values())
+    return round_to(got_points / max_points * 5 + 1)
 
 
 def extract_result(row, criteria):
@@ -33,3 +45,10 @@ def extract_result(row, criteria):
         except:
             result['misc'][criterium] = value
     return result
+
+
+def extract_criteria(title_row, from_col):
+    criteria = {}
+    for i, criterium in enumerate(title_row[from_col:]):
+        criteria[i + from_col] = criterium.value
+    return criteria
